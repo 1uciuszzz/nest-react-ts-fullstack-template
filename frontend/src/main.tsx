5058006268aa2f98ac88b5d21eceff86842e10d0
1uciuszzz <nick_zzhao@outlook.com>
2024-06-04 00:14:33 +0800

    api define、router config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router.tsx";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 80,
     proxy: {
       "/api": {
         target: "http://localhost:3000",
