029237ff9fd76f8cdc2ed9616d3a3ae3cba81b84
1uciuszzz <nick_zzhao@outlook.com>
2024-06-04 20:07:07 +0800

    bug fix

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,15 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 
-const dynImport = async (path: string) => {
-  const component = await import(path);
-  return {
-    Component: component.default,
-  };
-};
+
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    lazy: () => dynImport("./App.tsx"),
+    lazy: async () => ({Component: (await import("./App.tsx")).default}),
   },
 ]);
